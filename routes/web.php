e2898b9c249a1dcda944d09b0905c9ee241055a6
syarif-nur <syarif.nur92@gmail.com>
2023-05-24 18:02:29 +0800

    update privacy

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('privacy',function(){
+    return view('privacy');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
